a911e742ec85761dc9a298a6535f8180f94f08d0
Alvin Crespo <alvin.crespo@gmail.com>
2018-04-25 09:33:56 -0400

    Removes configs

--- config/dev.exs
+++
@@ -1,3 +0,0 @@
-use Mix.Config
-
-config :ueberauth, :oauth2_client, OAuth2.Client
--- config/test.exs
+++
@@ -1,8 +0,0 @@
-use Mix.Config
-
-config :ueberauth, :oauth2_client, Ueberauth.Client.OAuth2
-
-config :ueberauth, Ueberauth.Strategy.Slack.OAuth,
-  providers: [
-    slack: {Ueberauth.Strategy.Slack, []}
-  ]
